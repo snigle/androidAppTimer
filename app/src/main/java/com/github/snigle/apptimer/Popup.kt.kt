7b555fec26d9fdd73942ade0d3f6d98d0b7a57b4
Ludovic Lamarche <snigle64@gmail.com>
2023-11-25 07:25:43 +0100

    display popup and store apps state

--- app/src/main/java/com/github/snigle/apptimer/Popup.kt.kt
+++ app/src/main/java/com/github/snigle/apptimer/Popup.kt.kt
@@ -3,14 +3,9 @@ package com.github.snigle.apptimer
 import android.app.usage.UsageStats
 import android.app.usage.UsageStatsManager
 import android.content.Context
+import android.content.Intent
 import android.util.Log
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.torrydo.floatingbubbleview.FloatingBubbleListener
 import com.torrydo.floatingbubbleview.service.expandable.BubbleBuilder
 import com.torrydo.floatingbubbleview.service.expandable.ExpandableBubbleService
@@ -21,9 +16,13 @@ import kotlinx.coroutines.launch
 import java.util.Timer
 import java.util.TimerTask
 
-class Popup: ExpandableBubbleService() {
+class Popup : ExpandableBubbleService() {
 
-    override fun configBubble(): BubbleBuilder? {
+    private var currentAppInPopup: StartedApp = StartedApp("empty", 0, 0)
+    private val apps = mutableMapOf<String, StartedApp>()
+
+
+    override fun configBubble(): BubbleBuilder {
 
 
         return BubbleBuilder(this)
@@ -32,15 +31,21 @@ class Popup: ExpandableBubbleService() {
 
             // or our sweetie, Jetpack Compose
             .bubbleCompose {
-                Surface(
-                    modifier = Modifier.width(30.dp),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                    Button(onClick = { removeAll()}) {
-                        Text(text = "Close")
-                    }
-                }
+                PopupCompose(
+                    app = this.currentAppInPopup,
+                    setTimer = { app: StartedApp -> this.setTimer(app)},
+                    settingsIntent = {app : StartedApp ->
+
+                        GlobalScope.launch(Dispatchers.Main) {
+                            removeAll()
+                            val intent = Intent(applicationContext, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK;
+                            startActivity(intent)
+                        }
+
+                        apps.remove(app.packageName)
+                    })
+
             }
 
             // set style for the bubble, fade animation by default
@@ -55,10 +60,10 @@ class Popup: ExpandableBubbleService() {
             .enableAnimateToEdge(false)
 
             // set close-bubble view
-           // .closeBubbleView(ViewHelper.fromDrawable(this, com.torrydo.floatingbubbleview.R.drawable.ic_close_bubble, 60, 60))
+            // .closeBubbleView(ViewHelper.fromDrawable(this, com.torrydo.floatingbubbleview.R.drawable.ic_close_bubble, 60, 60))
 
             // set style for close-bubble, null by default
-           // .closeBubbleStyle(null)
+            // .closeBubbleStyle(null)
 
             // DYNAMIC_CLOSE_BUBBLE: close-bubble moving based on the bubble's location
             // FIXED_CLOSE_BUBBLE (default): bubble will automatically move to the close-bubble when it reaches the closable-area
@@ -71,17 +76,26 @@ class Popup: ExpandableBubbleService() {
             //.bottomBackground(true)
 
             .addFloatingBubbleListener(object : FloatingBubbleListener {
-                override fun onFingerMove(x: Float, y: Float) {} // The location of the finger on the screen which triggers the movement of the bubble.
-                override fun onFingerUp(x: Float, y: Float) {}   // ..., when finger release from bubble
+                override fun onFingerMove(
+                    x: Float,
+                    y: Float
+                ) {
+                } // The location of the finger on the screen which triggers the movement of the bubble.
+
+                override fun onFingerUp(
+                    x: Float,
+                    y: Float
+                ) {
+                }   // ..., when finger release from bubble
+
                 override fun onFingerDown(x: Float, y: Float) {} // ..., when finger tap the bubble
             })
     }
 
-    override fun configExpandedBubble(): ExpandedBubbleBuilder? {
+    override fun configExpandedBubble(): ExpandedBubbleBuilder {
 
 
-        return ExpandedBubbleBuilder(this)
-            .expandedCompose {
+        return ExpandedBubbleBuilder(this).expandedCompose {
                 Greeting("tata")
             }
             // handle key code
@@ -101,74 +115,73 @@ class Popup: ExpandableBubbleService() {
         Log.d("test", "restart service")
         enableBubbleDragging(false)
 
-        Timer().scheduleAtFixedRate(CustomTimerTask(this),0,1000)
+        Timer().scheduleAtFixedRate(
+            CustomTimerTask(
+                this,
+                callback = { app: StartedApp -> this.detectApp(app) }), 0, 1000
+        )
 
     }
 
-    private val timerTask: TimerTask = object : TimerTask() {
-        override fun run() {
-            Log.d("test", "timerrrrr : ")
-            //GlobalScope.launch(Dispatchers.Main) {
-            //            popup.minimize()
-            //        }
-            //expand()
-
+    private fun detectApp(lastApp: StartedApp): Unit {
+        // check if app is watached in user settings
+        if (lastApp.packageName == "com.github.snigle.apptimer" || lastApp.packageName.contains("launcher")) {
+            return
+        }
+        if (!apps.containsKey(lastApp.packageName)) {
+            Log.d("YourService", "detected app ${lastApp.packageName}")
+            apps[lastApp.packageName] = lastApp
+            this.askTimer(lastApp)
         }
     }
 
-
-
+    private fun askTimer(app: StartedApp) {
+        this.currentAppInPopup = app
+        //this.minimize()
+        var popup : Popup = this
+        GlobalScope.launch(Dispatchers.Main) {
+        popup.minimize()
+        }
     }
 
-class StartedApp(val packageName: String, duration : Long, startedAt: Long) {
+    private fun setTimer(app: StartedApp) {
+        apps[app.packageName] = app
+        this.removeAll()
+    }
+
+}
+
+class StartedApp(val packageName: String, duration: Long, startedAt: Long) {
     // StartTimer
 
     // HaveTimer
 }
 
-class CustomTimerTask(private val popup: Popup) : TimerTask() {
+class CustomTimerTask(private val popup: Popup, private val callback: (app: StartedApp) -> Unit) :
+    TimerTask() {
 
-    private val apps = mutableMapOf<String,StartedApp>()
+    private val apps = mutableMapOf<String, StartedApp>()
 
     override fun run() {
-        val lastApp = this.getLastStartedApp(popup)
-        if (!apps.containsKey(lastApp.packageName)) {
-            Log.d("YourService", "TimerTask is running: " + lastApp.packageName)
-            apps[lastApp.packageName] = lastApp
-        }
-        GlobalScope.launch(Dispatchers.Main) {
-            //popup.minimize()
-        }
+        this.callback(this.getLastStartedApp(popup))
+
+        //GlobalScope.launch(Dispatchers.Main) {
+        //popup.minimize()
+        //}
         // Here, you can use the context to perform any operations
         // that require the context within the TimerTask
     }
 
-    private fun getForegroundApp(context: Context): String {
-        val usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
-        val currentTime = System.currentTimeMillis()
-
-        val stats = usageStatsManager.queryUsageStats(
-            UsageStatsManager.INTERVAL_DAILY,
-            currentTime - 1000 * 10,  // Check for last 10 seconds
-            currentTime
-        )
-
-        stats?.let {
-            if (it.isNotEmpty()) {
-                return it[0].packageName
-            }
-        }
-
-        return "" // No foreground app found
-    }
 
     fun getLastStartedApp(context: Context): StartedApp {
         val currentTimestamp = System.currentTimeMillis()
-        val usageStatsManager = context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
+        val usageStatsManager =
+            context.getSystemService(Context.USAGE_STATS_SERVICE) as UsageStatsManager
 
         // Set the time window for querying usage statistics
         val oneHourAgo = currentTimestamp - (60 * 1000) // 1m ago
-        val appList: Map<String, UsageStats> = usageStatsManager.queryAndAggregateUsageStats(oneHourAgo, currentTimestamp)
+        val appList: Map<String, UsageStats> =
+            usageStatsManager.queryAndAggregateUsageStats(oneHourAgo, currentTimestamp)
 
         var lastUsedApp = ""
         var lastTimeStamp = 0L
---
+++ app/src/main/java/com/github/snigle/apptimer/PopupCompose.kt
@@ -0,0 +1,54 @@
+package com.github.snigle.apptimer
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.Instant
+
+@Composable
+fun PopupCompose(
+    modifier: Modifier = Modifier,
+    app: StartedApp,
+    setTimer: (app: StartedApp)->Unit,
+    settingsIntent: (app: StartedApp)->Unit,
+    ) {
+    return Surface(
+        modifier = Modifier.width(200.dp),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Row(  modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Text(text = "Add timer for ${app.packageName}")
+
+                Button(onClick = { setTimer(StartedApp(app.packageName, 5 * 60, Instant.now().epochSecond))}) {
+                    Text(text = "5 Minutes")
+                }
+                Button(onClick = { settingsIntent(app) }) {
+                    Text(text = "Settings")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PopupComposePreview() {
+return PopupCompose(app = StartedApp("test", 0,0), setTimer = { _ -> }, settingsIntent = {_ ->})
+}
