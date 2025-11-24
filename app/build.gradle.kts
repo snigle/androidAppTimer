59f6347f059e85d96b6fd8f616e43cace74483f6
Ludovic Lamarche <snigle64@gmail.com>
2025-11-24 22:04:43 +0100

    release 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.github.snigle.apptimer"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
